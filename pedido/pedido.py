b62f4fde7e9582816d0614cdd32f54d679d7eab5
Leonardo Melo <leonardo.melo@gdstecnologia.com.br>
2025-05-01 16:17:58 -0300

    feat: add Dependency Inversion Principle

--- pedido/pedido.py
+++ pedido/pedido.py
@@ -5,6 +5,16 @@ class Pedido(ABC):
     def __init__(self, cliente, itens):
         self.cliente = cliente
         self.itens = itens
+        self._status = "Criado!"
+
+    @property
+    def status(self):
+        return self._status
+    
+    @status.setter
+    def status(self, novo_status):
+        self._status = novo_status
+        print(f"Novo status do pedido: {self._status}")
 
     @abstractmethod
     def calcular_total(self):
